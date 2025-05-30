1f168ebd7a6c46ae453e9771bdeb527f63f8dea5
Dmitry Timashenko <timashenko.dmitry@gmail.com>
2025-05-30 14:29:23 +0300

    Задание #2

---
+++ m1/3_data_types/index.js
@@ -0,0 +1,17 @@
+let age = 42;
+console.log('age', age, typeof age);
+
+let myName = "Dmitry";
+console.log('name', myName, typeof myName);
+
+let isGudGuy = true;
+console.log('isGudGuy', isGudGuy, typeof isGudGuy);
+
+age = "Сорок два";
+alert(age);
+
+myName = 32;
+alert(32);
+
+isGudGuy = null;
+alert(isGudGuy);
