ad67d6cd1c19ec42eb9ba1103da67344159f51c4
Dmitry Timashenko <timashenko.dmitry@gmail.com>
2025-05-30 14:19:40 +0300

    first commit

---
+++ m1/3_data_types/data-types.js
@@ -0,0 +1,23 @@
+const age = 42;
+console.log('age', age, typeof age);
+
+const mySalary = 9999999999999999999999999999999999999999999999999999999n;
+console.log('my salary', mySalary, typeof mySalary);
+
+const myName = "Dmitry";
+console.log('name', myName, typeof myName);
+
+const isGudGuy = true;
+console.log('isGudGuy', isGudGuy, typeof isGudGuy);
+
+const unknown = undefined;
+console.log('unknown', unknown, typeof unknown);
+
+const empty = null;
+console.log('empty', empty, typeof empty);
+
+const id = Symbol('id');
+console.log('id', id, typeof id);
+
+const person = { city: 'Moscow' };
+console.log('person', person, typeof person);
